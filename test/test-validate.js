ced587af896449104ade938a26a725e3c59d0f78
quoc quach <quoc_cooc@yahoo.com>
2015-05-07 01:20:27 -0700

    adding extended validate for array and mixed base on swagger concept

--- test/test-validate.js
+++ test/test-validate.js
@@ -8,25 +8,61 @@ var Schema = mongoose.Schema;
 var log = require("../lib/log.js");
 
 module.exports = {
-  testPlugin: function(test){
+  testNumber: function(test){
     var schema = new Schema({
       aNumber: {
         type:Number,
         required: true,
         min: 5,
         max: 100
-      },
+      }
+    });
+    var Model = mongoose.model("TestNumber",schema);
+    var m = new Model({
+      aNumber: "13"
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testString: function(test){
+    var schema = new Schema({
       aString: {
         type: String,
         match: /\w{4,}/i,
         maxlength: 10,
         required: true,
         enum: ["asdf","b","c"]
-      },
+      }
+    });
+    var Model = mongoose.model("TestString",schema);
+    var m = new Model({
+      aString: "asdf"
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testBoolean: function(test){
+    var schema = new Schema({
       aBoolean: {
         type: Boolean,
         required: true
-      },
+      }
+    });
+    var Model = mongoose.model("TestBoolean",schema);
+    var m = new Model({
+      aBoolean: ''
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testObject: function(test){
+    var schema = new Schema({
       anObject: {
         num: {
           type: Number,
@@ -36,7 +72,22 @@ module.exports = {
           type: Date,
           required: true
         }
-      },
+      }
+    });
+    var Model = mongoose.model("TestObject",schema);
+    var m = new Model({
+      anObject: {
+        num: "123",
+        date: "2000-01-01"
+      }
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testDate: function(test){
+    var schema = new Schema({
       aDate: {
         type: Date,
         required: true,
@@ -46,51 +97,79 @@ module.exports = {
           return d;
         })(),
         min: new Date('01/01/1900')
-      },
+      }
+    });
+    var Model = mongoose.model("TestDate",schema);
+    var m = new Model({
+      aDate: new Date("1997-01-01")
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testArray: function(test){
+    var schema = new Schema({
       anArrayString: {
         type: Array,
         items: {
           type: "string",
           enum: ["a","b","c","d"]
         },
-        minlength: 5
-      },
+        minItems: 5
+      }
+    });
+    var Model = mongoose.model("TestArray",schema);
+    var m = new Model({
+      anArrayString: ["a","b","d","a","b","c"]
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testArrayObject: function(test){
+    var schema = new Schema({
       anArrayObject: {
         type: Array,
         items: {
-          key: {
-            type: "string",
-            enum:["a","b"],
-            required:true
-          },
-          value: {
-            type: "string",
-            required: true,
-            maxlength: 1,
+          type:{
+            key: {
+              type: "string",
+              enum:["a","b"],
+              required:true
+            },
+            value: {
+              type: "string",
+              required: true,
+              maxlength: 1,
+            }
           }
         },
-        minlength:2,
-        maxlength: 10
-      },
+        minItems:2,
+        maxItems: 10
+      }
+    });
+    var Model = mongoose.model("TestArrayObject",schema);
+    var m = new Model({
+      anArrayObject:
+        [{key:'a', value: {}},
+         {key:'b', value: 'B'}]
+    });
+    m.validate(function(e){
+      test.equals(e,null);
+    });
+    test.done();
+  },
+  testMixed: function(test){
+    var schema = new Schema({
       any1: Schema.Types.Mixed,
       any2: [],
       any3: [{}],
       any4: [Schema.Types.Mixed]
     });
-    var Model = mongoose.model("TestPlugin",schema);
+    var Model = mongoose.model("TestMixed",schema);
     var m = new Model({
-      aNumber: "13",
-      aString: "asdf",
-      aBoolean: '',
-      anObject: {
-        num: "123",
-        date: "2000-01-01"
-      },
-      aDate: new Date("1997-01-01"),
-      anArrayString: ["a","b"],
-      anArrayObject:
-        [{key:'a', value: {}},
-         {key:'b', value: 'B'}],
       any1: {
         what:'ever'
       },
@@ -99,7 +178,6 @@ module.exports = {
       any4: [1,"2",true]
     });
     m.validate(function(e){
-      log.debug("validate come back: %s", e.message);
       test.equals(e,null);
     });
     test.done();
